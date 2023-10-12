9cec393b01d36ca39e0f490d2a8fff4ab6f2949c
Neffrey <neffreyl@gmail.com>
2023-10-12 17:33:01 -0500

    Remove prettier-plugin-tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,14 @@
 // prettier.config.mjs
 
-/** @type {import("prettier").Config} */
+/** @type {import("prettier").Config  & import('prettier-plugin-tailwindcss').options} */
 config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
   tailwindFunctions: ["cva", "cn"],
   plugins: ["prettier-plugin-tailwindcss"],
 };
